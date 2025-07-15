04734c6dcfe24024bbc3cf852241541af3804c47
arsalan <arsahlahn3@gmail.com>
2025-07-15 16:11:21 +0200

    Initial Push

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import MapContainer from './components/MapContainer/MapContainer.jsx'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <MapContainer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/MapContainer/MapContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MapContainer = () => {
+    return (
+        <div className='map-container'>
+            <div id="map">
+                Map Container
+            </div>
+        </div>
+    )
+}
+
+export default MapContainer
