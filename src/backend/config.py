700cccb145ef7c934fb8cd7b4c6874dc0b3803aa
arsalan <arsahlahn3@gmail.com>
2025-07-17 09:53:39 +0200

    fixed modules issue

--- src/backend/config.py
+++ src/backend/config.py
@@ -11,6 +11,7 @@ class Settings:
     """
 
     DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DOCKER_DATABASE_URL: str = os.getenv("DOCKER_DATABASE_URL")
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
